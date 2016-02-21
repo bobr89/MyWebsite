f8c6da5efa1818fa787d56c4711b92a4eccafca2
Brian Chan <toyer889@gmail.com>
2016-02-21 12:24:57 -0500

    added index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+		<meta charset="UTF-8">
+		<title>Website</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+		<link rel="stylesheet" href="C:/Users/Brian/Desktop/W/style/index_style.css">
+		<script type="text/javascript" src="C:/Users/Brian/Desktop/W/js/script.js"></script>
+		
+</head>
+
+<body>
+	<header>
+		<nav>
+			<ul>
+				<a href='http://www.google.com'><li>About</li></a>
+				<a href='http://www.google.com'><li>Experience</li></a>
+				<a href='http://www.google.com'><li>Projects</li></a>
+			</ul>
+
+				<img id='logo' src='pictures/name.jpg'>
+
+			<ul>
+				<a href='http://www.google.com'><li>Experience</li></a>
+				<a href='http://www.google.com'><li>Contact</li></a>
+				<a href='http://www.google.com'><li>Interests</li></a>
+			</ul>
+		</nav>
+	</header>
+
+	<section id='banner'>
+
+	</section>
+
+		<section id='body'></section>
+
+	</body>
+</html>
